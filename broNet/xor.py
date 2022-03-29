8ae1363bbe1893dac3a9e07ad7af1e4546cd9b32
roshray <roshanroy759@gmail.com>
2022-03-29 22:29:06 +0530

    trail

---
+++ broNet/xor.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 roshan <roshan@roshan-ThinkPad-T440p>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import numpy as np
+
+from broNet.train import train
+from broNet.nn import NeuralNet
+from broNet.layers import Linear, Tanh
+
+
+inputs = np.array([
+    [0,0],
+    [1,0],
+    [0,1],
+    [1,0]
+])
+
+net = NeuralNet([
+    Linear(input_size=2, output_size=2)
+])
+
+train(net, inputs, targets)
+
+for x, y in zip(inputs, targets):
+    predicted = net.forward(x)
+    print(x, predicted, y)
+
+
+
+
+
+
