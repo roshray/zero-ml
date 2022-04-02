25e51622aaa6ae6c18dca9c6e41dfdbda661d109
roshray <roshanroy759@gmail.com>
2022-04-02 23:15:48 +0530

    trial 2

---
+++ toy.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 roshan <roshan@roshan-ThinkPad-T440p>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
